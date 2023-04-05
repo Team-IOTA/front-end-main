0239de822f5329d94b4ffd505720910d2f78547a
kasup wellege <124659511+Kasup0@users.noreply.github.com>
2023-04-06 02:06:14 +0530

    final

--- src/SecondPage/Player.js
+++ src/SecondPage/Player.js
@@ -3,6 +3,7 @@ import './Player.css';
 import Carousel from 'react-multi-carousel';
 import 'react-multi-carousel/lib/styles.css';
 import Loader from './Loader';
+import axios from 'axios';
 
 const Card = (props) => {
   return (
@@ -16,68 +17,68 @@ const Card = (props) => {
   );
 };
 
-
-const timestamps = [
-  {
-    image: "https://via.placeholder.com/150",
-    timestamp: "00:00:10",
-    topic: "Introduction",
-    timeInSeconds: 10,
-    summary: "Introduction"
-  },
-  {
-    image: "https://via.placeholder.com/150",
-    timestamp: "00:00:15",
-    topic: "Chapter 1",
-    timeInSeconds: 15,
-    summary: "Introduction 1"
-  },
-  {
-    image: "https://via.placeholder.com/150",
-    timestamp: "00:00:20",
-    topic: "Chapter 2",
-    timeInSeconds: 20,
-    summary: "Introduction 2"
-  },
-  {
-    image: "https://via.placeholder.com/150",
-    timestamp: "00:00:25",
-    topic: "Conclusion",
-    timeInSeconds: 25,
-    summary: "Introduction 3"
-  },
-  {
-    image: "https://via.placeholder.com/150",
-    timestamp: "00:00:30",
-    topic: "Conclusion",
-    timeInSeconds: 30,
-    summary: "Introduction 4"
-  },
-  {
-    image: "https://via.placeholder.com/150",
-    timestamp: "00:03:45",
-    topic: "Conclusion",
-    timeInSeconds: 225
-  },
-  {
-    image: "https://via.placeholder.com/150",
-    timestamp: "00:03:45",
-    topic: "Conclusion",
-    timeInSeconds: 225
-  },
-  {
-    image: "https://via.placeholder.com/150",
-    timestamp: "00:03:45",
-    topic: "Conclusion",
-    timeInSeconds: 225
-  },
-  {
-    image: "https://via.placeholder.com/150",
-    timestamp: "00:03:45",
-    topic: "Conclusion",
-    timeInSeconds: 225
-  }
-];
+// let timestamps=[]
+// let timestamps = [
+//   {
+//     image: "https://via.placeholder.com/150",
+//     timestamp: "00:00:10",
+//     topic: "Introduction",
+//     timeInSeconds: 10,
+//     summary: "Introduction"
+//   },
+//   {
+//     image: "https://via.placeholder.com/150",
+//     timestamp: "00:00:15",
+//     topic: "Chapter 1",
+//     timeInSeconds: 15,
+//     summary: "Introduction 1"
+//   },
+//   {
+//     image: "https://via.placeholder.com/150",
+//     timestamp: "00:00:20",
+//     topic: "Chapter 2",
+//     timeInSeconds: 20,
+//     summary: "Introduction 2"
+//   },
+//   {
+//     image: "https://via.placeholder.com/150",
+//     timestamp: "00:00:25",
+//     topic: "Conclusion",
+//     timeInSeconds: 25,
+//     summary: "Introduction 3"
+//   },
+//   {
+//     image: "https://via.placeholder.com/150",
+//     timestamp: "00:00:30",
+//     topic: "Conclusion",
+//     timeInSeconds: 30,
+//     summary: "Introduction 4"
+//   },
+//   {
+//     image: "https://via.placeholder.com/150",
+//     timestamp: "00:03:45",
+//     topic: "Conclusion",
+//     timeInSeconds: 225
+//   },
+//   {
+//     image: "https://via.placeholder.com/150",
+//     timestamp: "00:03:45",
+//     topic: "Conclusion",
+//     timeInSeconds: 225
+//   },
+//   {
+//     image: "https://via.placeholder.com/150",
+//     timestamp: "00:03:45",
+//     topic: "Conclusion",
+//     timeInSeconds: 225
+//   },
+//   {
+//     image: "https://via.placeholder.com/150",
+//     timestamp: "00:03:45",
+//     topic: "Conclusion",
+//     timeInSeconds: 225
+//   }
+// ];
 
 const Player = () => {
   const [isPlaying, setIsPlaying] = useState(false);
@@ -86,6 +87,53 @@ const Player = () => {
   const [currentTimestamp, setCurrentTimestamp] = useState(0);
   const [isLoading, setIsLoading] = useState(false);
   const videoPlayer = useRef(null);
+  const [selectedFile, setSelectedFile] = React.useState(null);
+  const [offers, setOffers] = useState([])
+  const [load, setLoad] = useState(false)
+
+  useEffect(() => {
+    // This should log offers to the console if it has been set
+    if(offers) {
+      console.log(offers,"check2")
+    }
+  }, [offers])
+
+  
+  const handleSubmit = async(event) => {
+    event.preventDefault()
+    setOffers([])
+    setLoad(true)
+    const formData = new FormData();
+    formData.append("video", selectedFile);
+    try {
+      const response = await axios({
+        method: "post",
+        url: "http://1006-2402-4000-2381-9586-b8fe-7fcc-8454-b32b.in.ngrok.io/api/getVideo",
+        data: formData,
+        headers: { "Content-Type": "multipart/form-data" },
+      });
+      console.log("check",response)
+      // timestamps=response.data.data
+      setOffers(response.data.data)
+      setLoad(false)
+    }
+     catch(error) {
+      console.log(error)
+      setLoad(false)
+    }
+  }
+  const handleFileSelect = (event) => {
+    setLoad(true)
+    setSelectedFile(event.target.files[0])
+    const filePath = event.target.files[0];
+    if (filePath) {
+      setLoad(false)
+      setIsLoading(true);
+      const file = URL.createObjectURL(filePath);
+      videoPlayer.current.src = file;
+      
+    }
+  }
 
 
   const handleTimeClick = (timeInSeconds) => {
@@ -94,14 +142,15 @@ const Player = () => {
   };
 
 
-  function handleFileChange(event) {
-    const filePath = event.target.files[0];
-    if (filePath) {
-      setIsLoading(true);
-      const file = URL.createObjectURL(filePath);
-      videoPlayer.current.src = file;
-    }
-  }
+  // function handleFileChange(event) {
+  //   const filePath = event.target.files[0];
+  //   if (filePath) {
+  //     setIsLoading(true);
+  //     const file = URL.createObjectURL(filePath);
+  //     videoPlayer.current.src = file;
+      
+  //   }
+  // }
 
   const handleVideoLoad = () => {
     setIsLoading(false);
@@ -129,10 +178,10 @@ const Player = () => {
 
     
     const handleTimeUpdate = (currentTime) => {
-      const newTimestamp = timestamps.findIndex(
+      const newTimestamp = offers.findIndex(
         (timestamp) => timestamp.timeInSeconds > currentTime
       );
-      setCurrentTimestamp(newTimestamp === -1 ? timestamps.length - 1 : newTimestamp);
+      setCurrentTimestamp(newTimestamp === -1 ? offers.length - 1 : newTimestamp);
     };
 
 
@@ -149,8 +198,10 @@ const Player = () => {
       
       <br />
       <br />
-      <div className="first-container">
-  {timestamps.map((timestamp, index) => (
+      {load &&<img src="https://media.tenor.com/v_OKGJFSkOQAAAAC/loading-gif.gif"></img>}
+      {offers &&<div className="first-container">
+      
+  {offers.map((timestamp, index) => (
     
       <div key={index} style={{ display: currentTimestamp === index ? 'block' : 'none' }}>
       <Card
@@ -162,21 +213,26 @@ const Player = () => {
     </div>
   ))}
 </div>
+}
 </div> 
       <br />
       <div class="wrapper">
   <label htmlFor="video-upload" >
     <div class="file-upload">
-      <input id="video-upload" type="file" accept="video/*" onChange={handleFileChange} />
+    <form onSubmit={handleSubmit}>
+      <input id="video-upload" type="file" accept="video/*" onChange={handleFileSelect} />
       <i class="fa fa-arrow-up"></i>
+      <input type="submit" value="Upload File" /></form>
+      
     </div>
   </label>
   {isLoading && <Loader />}
 </div>
   
       <Carousel responsive={responsive} style={{ width: '1000px' }} >
-      <div className="timestamp-container" style={{ width: '1000px' }}>
-        {timestamps.map((timestamp) => (
+     
+      {offers &&<div className="timestamp-container" style={{ width: '1000px' }}>
+        {offers.map((timestamp) => (
           <div className="timestamp-item" key={timestamp.timestamp}>
             
             <img src={`/${timestamp.image}`} alt={`Timestamp at ${timestamp.timestamp}`} onClick={() => handleTimeClick(timestamp.timeInSeconds)} style={{ cursor: "pointer" }} />
@@ -186,7 +242,7 @@ const Player = () => {
             </div>
           </div>
         ))}
-      </div>
+      </div>}
       </Carousel>;
       
     </div>
